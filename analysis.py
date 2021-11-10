5e0ad8eafe7afe39351982178bf2f7065b57e5db
souzamarcelo <marcelo.desouza@udesc.br>
2021-11-10 10:25:46 -0300

    New plots.

--- analysis.py
+++ analysis.py
@@ -58,25 +58,205 @@ def table_sample_cif_students(data):
     table = pd.concat(tables)
     return table
 
-# def plot_institution(data):
-#     total_publica = data[data['instituicao'] == 'publica']['instituicao'].count()
-#     total_privada = data[data['instituicao'] == 'privada']['instituicao'].count()
-#     data['conhece_s'] = data['conhece'].map(lambda x: 1 if x == 'S' else 0)
-#     data['conhece_n'] = data['conhece'].map(lambda x: 1 if x == 'N' else 0)
-#     data['usa_s'] = data['usa'].map(lambda x: 1 if x == 'S' else 0)
-#     data['usa_n'] = data['usa'].map(lambda x: 1 if x == 'N' else 0)
-#     data = data.groupby('instituicao', as_index = False).agg({'conhece_s': 'sum', 'conhece_n': 'sum', 'usa_s': 'sum', 'usa_n': 'sum'})
-#     data.loc[data['instituicao'] == 'publica', 'conhece_s'] = data[data['instituicao'] == 'publica']['conhece_s'].map(lambda x: 100 * x / total_publica)
-#     data.loc[data['instituicao'] == 'publica', 'conhece_n'] = data[data['instituicao'] == 'publica']['conhece_n'].map(lambda x: 100 * x / total_publica)
-#     data.loc[data['instituicao'] == 'privada', 'conhece_s'] = data[data['instituicao'] == 'privada']['conhece_s'].map(lambda x: 100 * x / total_privada)
-#     data.loc[data['instituicao'] == 'privada', 'conhece_n'] = data[data['instituicao'] == 'privada']['conhece_n'].map(lambda x: 100 * x / total_privada)
+def plot_institution(data):
+    data['conhece_s'] = data['conhece'].map(lambda x: 1 if x == 'S' else 0)
+    data['conhece_n'] = data['conhece'].map(lambda x: 1 if x == 'N' else 0)
+    data['usa_s'] = data['usa'].map(lambda x: 1 if x == 'S' else 0)
+    data['usa_n'] = data['usa'].map(lambda x: 1 if x == 'N' else 0)
+    data['importante_s'] = data['considera_importante'].map(lambda x: 1 if x == 'S' else 0)
+    data['importante_n'] = data['considera_importante'].map(lambda x: 1 if x == 'N' else 0)
+    data = data.groupby('instituicao', as_index = False).agg({'conhece_s': 'sum', 'conhece_n': 'sum', 'usa_s': 'sum', 'usa_n': 'sum', 'importante_s': 'sum', 'importante_n': 'sum'})
     
-#     plt.bar(data['instituicao'], data['conhece_s'])
-#     plt.bar(data['instituicao'], data['conhece_n'])
-#     plt.show()
+    publica_sim = [
+        data[data['instituicao'] == 'publica']['conhece_s'].unique()[0],
+        data[data['instituicao'] == 'publica']['importante_s'].unique()[0],
+        data[data['instituicao'] == 'publica']['usa_s'].unique()[0]
+    ]
+    publica_nao = [
+        data[data['instituicao'] == 'publica']['conhece_n'].unique()[0],
+        data[data['instituicao'] == 'publica']['importante_n'].unique()[0],
+        data[data['instituicao'] == 'publica']['usa_n'].unique()[0]
+    ]
+    privada_sim = [
+        data[data['instituicao'] == 'privada']['conhece_s'].unique()[0],
+        data[data['instituicao'] == 'privada']['importante_s'].unique()[0],
+        data[data['instituicao'] == 'privada']['usa_s'].unique()[0]
+    ]
+    privada_nao = [
+        data[data['instituicao'] == 'privada']['conhece_n'].unique()[0],
+        data[data['instituicao'] == 'privada']['importante_n'].unique()[0],
+        data[data['instituicao'] == 'privada']['usa_n'].unique()[0]
+    ]
+
+    fig = plt.figure()
+    labels = ['Conhece', 'Considera\nimportante', 'Usa']
+    width = 0.8
+
+    ax = fig.add_subplot(1, 2, 1)
+    ax.set_ylim(0, 50)
+    ax.bar(labels, publica_sim, width, label = 'Sim')
+    ax.bar(labels, publica_nao, width, bottom = publica_sim, label = 'Não')
+    ax.set_ylabel('Quantidade')
+    ax.set_title('Instituições públicas')
+    ax.legend()
+
+    ax = fig.add_subplot(1, 2, 2)
+    ax.set_ylim(0, 50)
+    ax.yaxis.tick_right()
+    ax.yaxis.set_label_position('right')
+    ax.bar(labels, privada_sim, width, label = 'Sim')
+    ax.bar(labels, privada_nao, width, bottom = privada_sim, label = 'Não')
+    ax.set_ylabel('Quantidade')
+    ax.set_title('Instituições privadas')
+    ax.legend()
+
+    fig.tight_layout()
+    return plt
+
+def plot_year(data):
+    data['conhece_s'] = data['conhece'].map(lambda x: 1 if x == 'S' else 0)
+    data['conhece_n'] = data['conhece'].map(lambda x: 1 if x == 'N' else 0)
+    data['usa_s'] = data['usa'].map(lambda x: 1 if x == 'S' else 0)
+    data['usa_n'] = data['usa'].map(lambda x: 1 if x == 'N' else 0)
+    data['importante_s'] = data['considera_importante'].map(lambda x: 1 if x == 'S' else 0)
+    data['importante_n'] = data['considera_importante'].map(lambda x: 1 if x == 'N' else 0)
+    data = data.groupby('periodo', as_index = False).agg({'conhece_s': 'sum', 'conhece_n': 'sum', 'usa_s': 'sum', 'usa_n': 'sum', 'importante_s': 'sum', 'importante_n': 'sum'})
+    
+    sim_3 = [
+        data[data['periodo'] == 3]['conhece_s'].unique()[0],
+        data[data['periodo'] == 3]['importante_s'].unique()[0],
+        data[data['periodo'] == 3]['usa_s'].unique()[0]
+    ]
+    nao_3 = [
+        data[data['periodo'] == 3]['conhece_n'].unique()[0],
+        data[data['periodo'] == 3]['importante_n'].unique()[0],
+        data[data['periodo'] == 3]['usa_n'].unique()[0]
+    ]
+    sim_4 = [
+        data[data['periodo'] == 4]['conhece_s'].unique()[0],
+        data[data['periodo'] == 4]['importante_s'].unique()[0],
+        data[data['periodo'] == 4]['usa_s'].unique()[0]
+    ]
+    nao_4 = [
+        data[data['periodo'] == 4]['conhece_n'].unique()[0],
+        data[data['periodo'] == 4]['importante_n'].unique()[0],
+        data[data['periodo'] == 4]['usa_n'].unique()[0]
+    ]
+    sim_5 = [
+        data[data['periodo'] == 5]['conhece_s'].unique()[0],
+        data[data['periodo'] == 5]['importante_s'].unique()[0],
+        data[data['periodo'] == 5]['usa_s'].unique()[0]
+    ]
+    nao_5 = [
+        data[data['periodo'] == 5]['conhece_n'].unique()[0],
+        data[data['periodo'] == 5]['importante_n'].unique()[0],
+        data[data['periodo'] == 5]['usa_n'].unique()[0]
+    ]
+
+    fig = plt.figure()
+    labels = ['Conhece', 'Considera\nimportante', 'Usa']
+    width = 0.8
+    max_lim = 40
+
+    ax = fig.add_subplot(1, 3, 1)
+    ax.set_ylim(0, max_lim)
+    ax.bar(labels, sim_3, width, label = 'Sim')
+    ax.bar(labels, nao_3, width, bottom = sim_3, label = 'Não')
+    ax.set_ylabel('Quantidade')
+    ax.set_title('3º ano')
+    ax.legend()
+
+    ax = fig.add_subplot(1, 3, 2)
+    ax.set_ylim(0, max_lim)
+    #ax.yaxis.tick_right()
+    #ax.yaxis.set_label_position('right')
+    ax.bar(labels, sim_4, width, label = 'Sim')
+    ax.bar(labels, nao_4, width, bottom = sim_4, label = 'Não')
+    ax.get_yaxis().set_visible(False)
+    ax.set_title('4º ano')
+    ax.legend()
+    
+    ax = fig.add_subplot(1, 3, 3)
+    ax.set_ylim(0, max_lim)
+    ax.yaxis.tick_right()
+    ax.yaxis.set_label_position('right')
+    ax.bar(labels, sim_5, width, label = 'Sim')
+    ax.bar(labels, nao_5, width, bottom = sim_5, label = 'Não')
+    ax.set_ylabel('Quantidade')
+    ax.set_title('5º ano')
+    ax.legend()
+
+    fig.set_size_inches(8.5, 4)
+    fig.tight_layout()
+    return plt
+
+def plot_knowledge(data):
+    com = []
+    sem = []
+    com.append(len(data[(data['treinamento'] == 'S') & (data['conhecimento'] == 'Muito ruim')]))
+    com.append(len(data[(data['treinamento'] == 'S') & (data['conhecimento'] == 'Ruim')]))
+    com.append(len(data[(data['treinamento'] == 'S') & (data['conhecimento'] == 'Razoável')]))
+    com.append(len(data[(data['treinamento'] == 'S') & (data['conhecimento'] == 'Bom')]))
+    com.append(len(data[(data['treinamento'] == 'S') & (data['conhecimento'] == 'Muito bom')]))
+    sem.append(len(data[(data['treinamento'] == 'N') & (data['conhecimento'] == 'Muito ruim')]))
+    sem.append(len(data[(data['treinamento'] == 'N') & (data['conhecimento'] == 'Ruim')]))
+    sem.append(len(data[(data['treinamento'] == 'N') & (data['conhecimento'] == 'Razoável')]))
+    sem.append(len(data[(data['treinamento'] == 'N') & (data['conhecimento'] == 'Bom')]))
+    sem.append(len(data[(data['treinamento'] == 'N') & (data['conhecimento'] == 'Muito bom')]))
+
+    fig = plt.figure()
+    ax = plt.gca()
+
+    labels = ['Muito ruim', 'Ruim', 'Razoável', 'Bom', 'Muito bom']
+    x = np.arange(len(labels))
+    width = 0.35
+
+    rects1 = ax.bar(x - width/2, sem, width, label = 'Sem treinamento', color = 'red')
+    rects2 = ax.bar(x + width/2, com, width, label = 'Com treinamento', color = 'green')
+
+    ax.set_ylabel('Quantidade')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+
+    ax.bar_label(rects1, padding = 2)
+    ax.bar_label(rects2, padding = 2)
+
+    fig.tight_layout()
+    return plt
+
+def plot_reason(data):
+    com = []
+    sem = []
+    com.append(len(data[(data['treinamento'] == 'S') & (data['motivo_nao_usa'] == 'Não conhecia essa classificação')]))
+    com.append(len(data[(data['treinamento'] == 'S') & (data['motivo_nao_usa'] == 'Tenho dificuldades para entender a CIF')]))
+    com.append(len(data[(data['treinamento'] == 'S') & (data['motivo_nao_usa'] == 'Tenho dificuldades para aplicar a CIF')]))
+    com.append(len(data[(data['treinamento'] == 'S') & (data['motivo_nao_usa'] == 'Eu uso a CIF')]))
+    sem.append(len(data[(data['treinamento'] == 'N') & (data['motivo_nao_usa'] == 'Não conhecia essa classificação')]))
+    sem.append(len(data[(data['treinamento'] == 'N') & (data['motivo_nao_usa'] == 'Tenho dificuldades para entender a CIF')]))
+    sem.append(len(data[(data['treinamento'] == 'N') & (data['motivo_nao_usa'] == 'Tenho dificuldades para aplicar a CIF')]))
+    sem.append(len(data[(data['treinamento'] == 'N') & (data['motivo_nao_usa'] == 'Eu uso a CIF')]))
+
+    fig = plt.figure()
+    ax = plt.gca()
+
+    labels = ['Não conhece', 'Dificuldade\npara entender', 'Dificuldade\npara aplicar', 'Usa']
+    x = np.arange(len(labels))
+    width = 0.35
+
+    rects1 = ax.bar(x - width/2, sem, width, label = 'Sem treinamento', color = 'red')
+    rects2 = ax.bar(x + width/2, com, width, label = 'Com treinamento', color = 'green')
+
+    ax.set_ylabel('Quantidade')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+
+    ax.bar_label(rects1, padding = 2)
+    ax.bar_label(rects2, padding = 2)
+
+    fig.tight_layout()
+    return plt
 
 #data = read_data_students()
-#x = table_sample_cif_students(data)
-#print(x)
-#table_sample_students(data)
-#exit()
+#plot_year(data)
